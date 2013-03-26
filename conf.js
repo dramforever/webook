bf1ca70629316a9e8993244c9f3544d99c3f2905
dramforever <wforever1@gmail.com>
2013-03-26 16:50:56 +0800

    Not working partial

---
+++ conf.js
@@ -0,0 +1,6 @@
+choose = "test1" ;
+lists = {
+	"test1":["test1 1","test2 2"],
+	"test2":["test2 1","test2 2"]
+}
+window.list = lists[choose];
---
+++ core.js
@@ -0,0 +1,29 @@
+var running = false,leng = 0,textEle;
+function show(txt)
+{
+	textEle.innerHTML = txt;
+}
+function toggle()
+{
+	running = !running;
+}
+function choose(){
+	console.log("In function choose");
+	if(!running)return;
+	var rand = Math.round(Math.random()*leng);
+	show(window.list[rand]);
+}
+function init(){
+	if(
+		typeof window.list != "array" ||
+		window.list.length == 0
+	)
+	{
+		alert("conf.js中有错误");
+		return;
+	}
+	leng = window.list.length;
+	textEle = document.getElementById("inner");
+	document.getElementById("inner").addEventListener("click",toggle,false);
+	setInterval(10 , choose);
+}
