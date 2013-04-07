aedf79a4a924142e12a7a4f8cdd0fc5ff5420057
dramforever <catmimiao@me.com>
2013-04-07 21:09:17 +0800

    Changed to add number as part of the li innerHTML

--- core.js
+++ core.js
@@ -21,12 +21,13 @@ function init(){
 	textEle = document.getElementById("inner");
 	document.getElementById("inner").onclick = toggle;
 	listsEle = document.getElementById("lists");
+	var x = 1;
 	for(i in window.lists)
 	{
 		var li = document.createElement("li");
-		li.innerHTML = i;
+		li.innerHTML = x.toString + ". " + i;
 		li.onclick = function(){
-			window.list = window.lists[this.innerHTML];
+			window.list = window.lists[this.innerHTML.replace(/^\d+?\. /,"")];
 			start();
 			var selEle = document.getElementById("sel");
 			selEle.style.display = "none";
